545a0d10756e0e023b811007a54a5f5b1862199e
Jeremiah Fischer <fischgeek@gmail.com>
2019-11-05 20:35:17 -0600

    quick

---
+++ AutoTogglConsole/Program.cs
@@ -0,0 +1,131 @@
+﻿using SharedLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static SharedLibrary.ConsoleShortcuts;
+
+namespace AutoTogglConsole
+{
+    class Program : _Base
+    {
+        private static TogglBase tb = TogglBase.GetInstance();
+        public static string lastActive = string.Empty;
+        public static bool idle = false;
+        public static bool aTimerIsRunning = false;
+        public static Project aProject = new Project() {
+            name = "Project Title"
+            , pid = 0000000 // get from toggl.com/app/projects
+            , keywords = new List<string>() {
+                "keyword"
+                , "keyword2"
+                , "keyword3"
+            }
+        };
+        public static List<string> neutralWindows = new List<string>() {
+            "Task Switching"
+            , "Windows PowerShell"
+            , "Administrator: Windows PowerShell"
+            , "Administration - Google Chrome"
+        };
+        public static List<Project> projects = new List<Project>() {
+            aProject
+        };
+
+        static void Main(string[] args)
+        {
+            handler = new ConsoleEventDelegate(ConsoleEventCallback);
+            SetConsoleCtrlHandler(handler, true);
+            tb.Init("Base64(APITOKEN:api_token)"); // Base64 encode your api token (toggl.com/app/profile
+            CheckForARunningTimer();
+            while (true) {
+                CheckIdleTime();
+                CheckActiveWindow();
+                System.Threading.Thread.Sleep(5000);
+            }
+        }
+
+        private static void CheckForARunningTimer()
+        {
+            var te = tb.GetRunningTimer();
+            if (te != null) {
+                clt($"Current running timer is {te.description}. Duration {tb.CurrentTimerDuration()}");
+                aTimerIsRunning = true;
+            }
+        }
+
+        private static void CheckActiveWindow()
+        {
+            if (!idle) {
+                var currentActive = GetActiveWindowTitle();
+                if (CurrentActiveIsValid(currentActive)) {
+                    clt(currentActive);
+                    var anyMatches = false;
+                    foreach (var project in projects) {
+                        if (KeywordExistsInActiveWindowTitle(project, currentActive)) {
+                            StartTimer(project, currentActive);
+                            anyMatches = true;
+                            break;
+                        }
+                    }
+                    if (!anyMatches && aTimerIsRunning) {
+                        clt("Window doesn't match any keywords in any projects. Stopping timer.");
+                        tb.StopRunningTimer();
+                        aTimerIsRunning = false;
+                    }
+                    lastActive = currentActive;
+                }
+            }
+        }
+
+        private static bool CurrentActiveIsValid(string currentActive) => lastActive != currentActive && currentActive.JFIsNotNull() && !neutralWindows.Contains(currentActive);
+
+        private static bool KeywordExistsInActiveWindowTitle(Project project, string currentActive)
+        {
+            foreach (var keyword in project.keywords) {
+                var match = Regex.Match(currentActive, keyword, RegexOptions.IgnoreCase);
+                if (match.Success) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private static void CheckIdleTime()
+        {
+            if (GetLastInputTime() >= 60) {
+                if (!idle) {
+                    clt("System idle. Stopping timer.");
+                    tb.StopRunningTimer();
+                    idle = true;
+                    lastActive = string.Empty;
+                    aTimerIsRunning = false;
+                }
+            } else {
+                idle = false;
+            }
+        }
+
+        private static void StartTimer(Project project, string description = "")
+        {
+            var ct = tb.GetRunningTimer();
+            if (ct != null && ct.pid == project.pid && ct.description == description) {
+                clt($"A timer is already running for {project.name}.");
+            } else {
+                TimeEntryWrapper wrapper = new TimeEntryWrapper();
+                wrapper.time_entry = new TimeEntry() {
+                    description = description
+                    , wid = 0000000 // your workspace id (toggl.com/app/workspaces)
+                    , pid = project.pid
+                    , created_with = ".net"
+                };
+                tb.StartTimer(wrapper);
+                clt($"Tracking started for {project.name}.");
+            }
+            aTimerIsRunning = true;
+        }
+    }
+}
